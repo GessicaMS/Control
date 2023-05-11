b69b074f5bee92f303cda45a2f4a3865c752b9c8
GessicaMS <gssk17@gmail.com>
2023-05-10 21:19:39 -0300

    Criado telas de login, cadastro e perfil de usuario. Feito implementação no firebase. Começado codigo de cadastro e autenticação de usuários.

---
+++ app/src/main/java/com/gessica/projetocontrol/TelaPerfil.java
@@ -0,0 +1,16 @@
+package com.gessica.projetocontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaPerfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_perfil);
+
+        getSupportActionBar().hide();
+    }
+}
