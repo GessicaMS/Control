7e62d6403c6ccaf4ce1dba0518bcdb5f24d6c898
Gessica <gssk17@gmail.com>
2023-06-14 21:56:46 -0300

    Configurado cadastro e autenticacao de usuarios, e recuperacao dos dados na tela de perfil.

--- app/src/main/java/com/gessica/projetocontrol/FormCadastro.java
+++ app/src/main/java/com/gessica/projetocontrol/FormCadastro.java
@@ -5,21 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class FormCadastro extends AppCompatActivity {
 
     private EditText edit_nome, edit_email, edit_senha;
     private Button bt_cadastrar;
     String [] mensagens = {"Preencha todos os campos", "Cadastro realizado com sucesso"};
+    String usuarioID;
 
 
 
@@ -45,7 +57,7 @@ public class FormCadastro extends AppCompatActivity {
                     snackbar.setBackgroundTint(Color.WHITE);
                     snackbar.setTextColor(Color.BLACK);
                     snackbar.show();
-                } else { // Estando preenchido os campos corretamente, usuario vai ser cadastrado, e mostrará a mensagem 2 de sucessso.
+                } else { // Estando preenchido os campos corretamente, usuario vai ser cadastrado, e mostrará a mensagem de sucessso.
                     CadastrarUsuario(view);
 
                 }
@@ -63,20 +75,61 @@ public class FormCadastro extends AppCompatActivity {
             public void onComplete(@NonNull Task<AuthResult> task) {
 
                 if (task.isSuccessful()) {
+
+                    SalvarDadosUsuario();
+
                     Snackbar snackbar = Snackbar.make(view,mensagens[1],Snackbar.LENGTH_SHORT);
                     snackbar.setBackgroundTint(Color.WHITE);
                     snackbar.setTextColor(Color.BLACK);
                     snackbar.show();
-                }
+                } else {
+                    String erro;
+                    try {
+                        throw task.getException();
 
+                    } catch (FirebaseAuthWeakPasswordException e) {
+                        erro = "Dígite uma senha com no mínimo 6 caracteres";
+                    }catch (FirebaseAuthUserCollisionException e) {
+                        erro = "Essa conta já foi cadastrada";
+                    }catch (FirebaseAuthInvalidCredentialsException e) {
+                        erro = "E-mail inválido";
+                    }catch (Exception e) {
+                        erro = "Erro ao cadastrar usuário";
+                    }
+
+                    Snackbar snackbar = Snackbar.make(view,erro,Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }
             }
         });
-
-
     }
 
-    private void IniciarComponentes() {
+    private void SalvarDadosUsuario() {
+        String nome = edit_nome.getText().toString();
 
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+
+        Map<String, Object> usuarios = new HashMap<>();
+        usuarios.put("nome", nome);
+
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DocumentReference documentReference = database.collection("Usuarios").document(usuarioID);
+        documentReference.set(usuarios).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Log.d("database", "Sucesso ao salvar os dados");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("database","Erro ao salvar os dados");
+            }
+        });
+    }
+    private void IniciarComponentes() {
         edit_nome = findViewById(R.id.edit_nome);
         edit_email = findViewById(R.id.edit_email);
         edit_senha = findViewById(R.id.edit_senha);
